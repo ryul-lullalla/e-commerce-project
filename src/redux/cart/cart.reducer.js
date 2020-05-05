728d4ce3cde4fe9a34a6be7e9555a6613ee82054
Jouz-FE <fr2d2ric.ma11e@gmail.com>
2020-05-05 23:26:25 +0900

    completed w/ managing state when clicking each single cart-item with redux

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,6 +2,7 @@ import CartActionTypes from './cart.types';
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden,
         // hidden: !action.payload,
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
       };
     default:
       return state;
