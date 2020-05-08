eb21788e1872e43f18dcc4835367cf84d27812f8
Jouz-FE <fr2d2ric.ma11e@gmail.com>
2020-05-08 23:53:09 +0900

    applied redux-persist to maintain the item data in local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,24 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+// sessionStorage: import storageSession from 'redux-persist/lib/storage/session'
+// localStorage: import storage from 'redux-persist/lib/storage'
+
+// importing actual local storage object on window browser: essingtially telling redux-persist using local storage as default storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
